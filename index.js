c64e93b90c36c579da77b49d49dce72d8dad68ab
rsomavi <rubensomavillagarcia@gmail.com>
2026-09-23 16:21:13 +0200

    add frontend production build

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
   { 
